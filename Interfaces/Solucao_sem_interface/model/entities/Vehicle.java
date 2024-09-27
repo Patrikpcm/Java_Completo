677668db8d3fd252b137c5efa2cda5a81be2a13b
patrikpcm <patrikpcm@hotmail.com>
2024-09-27 17:02:27 -0300

    Exemplos de java - Interfaces

---
+++ Interfaces/Solucao_sem_interface/model/entities/Vehicle.java
@@ -0,0 +1,17 @@
+package Interfaces.Solucao_sem_interface.model.entities;
+
+public class Vehicle {
+    private String model;
+
+    public Vehicle(String model) {
+        this.model = model;
+    }
+    public Vehicle(){}
+
+    public String getModel() {
+        return model;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
+}
