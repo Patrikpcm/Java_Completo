4572db5f2c4f7ba8f11608325cad45d5a95122d2
patrikpcm <patrikpcm@hotmail.com>
2024-10-18 11:34:54 -0300

    Exemplos de java

--- JDBC/ProjetoDAO/src/model/dao/DaoFactory.java
+++ JDBC/ProjetoDAO/src/model/dao/DaoFactory.java
@@ -1,6 +1,7 @@
 package JDBC.ProjetoDAO.src.model.dao;
 
 import JDBC.ProjetoDAO.src.db.DB;
+import JDBC.ProjetoDAO.src.model.dao.impl.DepartmentDaoJDBC;
 import JDBC.ProjetoDAO.src.model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
@@ -8,4 +9,8 @@ public class DaoFactory {
     public static SellerDao createSellerDao(){
         return new SellerDaoJDBC(DB.getConnection());
     }
+    
+    public static DepartmentDao createDepartmentDao() {
+		return new DepartmentDaoJDBC(DB.getConnection());
+	}
 }
